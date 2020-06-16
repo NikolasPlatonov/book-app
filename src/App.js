fcefd4b21a21a7a44ef4e3d2b7fb0085b8cd528f
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-06-17 00:30:53 +0300

    Module CSS added Books details displayed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import BookItem from './components/BookItem';
 import Details from './components/Details';
-import Favorits from './components/Favorits';
-import { Grid, Button, Search } from 'semantic-ui-react';
+import { Button, Search } from 'semantic-ui-react';
 import autoBind from 'react-autobind';
 
 class App extends Component {
@@ -38,73 +37,45 @@ class App extends Component {
     });
   };
 
-  // filteredBooks = (data) => {
-  //   return data.filter((text) => {
-  //     return (
-  //       text.volumeInfo.authors[0]
-  //         .toLowerCase()
-  //         .indexOf(this.state.inputText.toLowerCase()) !== -1 ||
-  //       text.volumeInfo.title
-  //         .toLowerCase()
-  //         .indexOf(this.state.inputText.toLowerCase()) !== -1 ||
-  //       text.volumeInfo.publishedDate
-  //         .toLowerCase()
-  //         .indexOf(this.state.inputText.toLowerCase()) !== -1 ||
-  //       null
-  //     );
-  //   });
-  // };
-
   render() {
     console.log('BOOKS-LIST', this.state.data);
     return (
-      <div className="App">
-        <Grid row={2} divided>
-          <Grid.Row>
-            <div className="searchBar">
-              <Search
-                type="text"
-                value={this.state.inputText}
-                // onSearchChange={this.onChange}
-                open={false}
-              />
-            </div>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid columns={3}>
-              <Grid.Column width={3}>
-                <div>
-                  {this.state.loading || !this.state.data
-                    ? 'LOADING...'
-                    : this.state.data.map((item, id) => {
-                        return (
-                          <div key={id} className="item">
-                            <Button
-                              onClick={(e) => this.detailsOpen(item, e)}
-                              className="ui segment"
-                            >
-                              <BookItem
-                                title={item.volumeInfo.title}
-                                authors={item.volumeInfo.authors}
-                                cover={item.volumeInfo.imageLinks.thumbnail}
-                              />
-                            </Button>
-                          </div>
-                        );
-                      })}
-                </div>
-              </Grid.Column>
-              <Grid.Column width={10}>
-                {this.state.details ? (
-                  <Details details={this.state.details} />
-                ) : (
-                  'Select a book from the list'
-                )}
-              </Grid.Column>
-              <Grid.Column width={3}>FAVORITES</Grid.Column>
-            </Grid>
-          </Grid.Row>
-        </Grid>
+      <div className="container">
+        <div className="search">
+          <Search
+            type="text"
+            value={this.state.inputText}
+            onSearchChange={this.onChange}
+            open={false}
+          />
+        </div>
+        <div className="content">
+          <div className="books_list">
+            {this.state.loading || !this.state.data
+              ? 'LOADING...'
+              : this.state.data.map((item, id) => {
+                  return (
+                    <div key={id} className="item">
+                      <button onClick={(e) => this.detailsOpen(item, e)}>
+                        <BookItem
+                          title={item.volumeInfo.title}
+                          authors={item.volumeInfo.authors}
+                          cover={item.volumeInfo.imageLinks.thumbnail}
+                        />
+                      </button>
+                    </div>
+                  );
+                })}
+          </div>
+          <div className="details">
+            {this.state.details ? (
+              <Details details={this.state.details} />
+            ) : (
+              'Select a book from the list'
+            )}
+          </div>
+          <div className="favorits"></div>
+        </div>
       </div>
     );
   }
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,36 +1,19 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
-import '../App.css';
+import s from './Details.module.css';
 
 const Details = (props) => {
   console.log('Details -> props', props);
   return (
-    <div className="fixed-details">
-      <Grid>
-        <Grid.Row>
-          <div className="common-details">
-            <Image
-              alt="book_cover"
-              width={100}
-              height={100}
-              src={props.details.volumeInfo.imageLinks.thumbnail}
-            />
-          </div>
-        </Grid.Row>
-        <Grid.Row>
-          <div className="bold-details">{props.details.volumeInfo.title}</div>
-        </Grid.Row>
-        <Grid.Row>
-          <div className="common-details">
-            {props.details.volumeInfo.authors}
-          </div>
-        </Grid.Row>
-        <Grid.Row>
-          <div className="common-details">
-            {props.details.volumeInfo.publisher}
-          </div>
-        </Grid.Row>
-      </Grid>
+    <div className={s.container}>
+      <div>
+        <img
+          alt="book_cover"
+          src={props.details.volumeInfo.imageLinks.thumbnail}
+        />
+      </div>
+      <div className="bold-details">{props.details.volumeInfo.title}</div>
+      <div className="common-details">{props.details.volumeInfo.authors}</div>
+      <div className="common-details">{props.details.volumeInfo.publisher}</div>
     </div>
   );
 };
