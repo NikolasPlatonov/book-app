de875db38d65e22f17809004cf6839ae33bae65f
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-06-22 12:58:28 +0300

    New pagination style added

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,24 +2,20 @@ import React from 'react';
 import '../App.css';
 import Pagination from 'react-bootstrap/Pagination';
 
+let active = 2;
+let items = [];
+for (let number = 1; number <= 5; number++) {
+  items.push(
+    <Pagination.Item key={number} active={number === active}>
+      {number}
+    </Pagination.Item>
+  );
+}
+
 const PaginationComponent = () => {
   return (
     <div>
-      <Pagination>
-        <Pagination.First />
-        <Pagination.Prev />
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis />
-
-        <Pagination.Item>{10}</Pagination.Item>
-        <Pagination.Item disabled>{11}</Pagination.Item>
-        <Pagination.Item active>{12}</Pagination.Item>
-
-        <Pagination.Ellipsis />
-        <Pagination.Item>{20}</Pagination.Item>
-        <Pagination.Next />
-        <Pagination.Last />
-      </Pagination>
+      <Pagination size="sm">{items}</Pagination>
     </div>
   );
 };
